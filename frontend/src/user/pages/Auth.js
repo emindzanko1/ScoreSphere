79d40e4b45d9e515da5dae340d3132133cb7ceff
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-13 20:29:39 +0200

    Login and registration are working, styled as before

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,52 +1,67 @@
 import React, { useState, useContext } from 'react';
+
 import { GoogleLogin } from 'react-google-login';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import AppleLogin from 'react-apple-login';
 import { FaGoogle, FaFacebook, FaApple } from 'react-icons/fa';
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import AuthContext from '../../shared/context/auth-context';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-
 import './Auth.css';
 
 const Auth = () => {
   const auth = useContext(AuthContext);
-
   const [isLoginMode, setIsLoginMode] = useState(true);
   const [passwordMatch, setPasswordMatch] = useState(true);
 
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    agreeTerms: false,
-  });
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: '',
+        isValid: false,
+      },
+      password: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false
+  );
 
   const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
     setIsLoginMode(prevMode => !prevMode);
   };
 
-  const changeHandler = event => {
-    const { name, value, type, checked } = event.target;
-    const newValue = type === 'checkbox' ? checked : value;
+  const authSubmitHandler = event => {
+    event.preventDefault();
 
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      [name]: newValue,
-    }));
-
-    if (name === 'confirmPassword') {
-      setPasswordMatch(formData.password === value);
+    if (!passwordMatch) {
+      return;
     }
-  };
 
-  const togglePasswordVisibility = () => {
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      showPassword: !prevFormData.showPassword,
-    }));
+    console.log(formState.inputs);
+    auth.login();
   };
 
   const responseGoogle = response => {
@@ -61,96 +76,50 @@ const Auth = () => {
     console.log(response);
   };
 
-  const submitHandler = event => {
-    event.preventDefault();
-
-    if (formData.password !== formData.confirmPassword) {
-      setPasswordMatch(false);
-      return;
-    }
-
-    // fetch('http://localhost:5000/users/register',{
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     name,
-    //     email,
-    //     password
-    //   })
-    // })
-
-    console.log(formData);
-    auth.login();
-  };
-
   return (
     <div className='signin-form-container'>
       {isLoginMode ? <h2>Sign In</h2> : <h2>Create an Account</h2>}
-      <form className='signin-form' onSubmit={submitHandler}>
-        {!isLoginMode ? (
-          <div className='form-group'>
-            {/*<label htmlFor="firstName">First Name</label>*/}
-            <input
-              element='input'
-              id='name'
-              //type=''Yor Name''
-              label='name'
-              //value={formData.fullName}
-              //onChange={handleChange}
-              required
-              //className='registration-form-input'
-              className='input-field'
-              placeholder='Your Name'
-              onInput={changeHandler}
-              // onInvalid={e => e.target.setCustomValidity('Please enter your name')}
-              // onInput={e => e.target.setCustomValidity('')}
-            />
-          </div>
-        ) : (
-          ''
+      <hr />
+      <form className='signin-form' onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element='input'
+            id='name'
+            type='text'
+            label='Your Name'
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText='Please enter a name.'
+            onInput={inputHandler}
+          />
         )}
-        <div className='form-group'>
-          <input
-            element='input'
-            id='email'
-            type='email'
-            label='E-Mail'
-            required
-            className='input-field'
-            placeholder='Email Address'
-            onInput={changeHandler}
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            element='input'
-            id='password'
-            type={formData.showPassword ? 'text' : 'password'}
-            label='Password'
-            required
-            className='input-field'
-            placeholder='Password'
-            onInput={changeHandler}
-          />
-          <FontAwesomeIcon
-            icon={formData.showPassword ? faEye : faEyeSlash}
-            className='password-icon'
-            onClick={togglePasswordVisibility}
-          />
-        </div>
-        {!isLoginMode ? (
+        <Input
+          element='input'
+          id='email'
+          type='email'
+          label='E-Mail'
+          validators={[VALIDATOR_EMAIL()]}
+          errorText='Please enter a valid email address.'
+          onInput={inputHandler}
+        />
+        <Input
+          element='input'
+          id='password'
+          type='password'
+          label='Password'
+          validators={[VALIDATOR_MINLENGTH(6)]}
+          errorText='Please enter a valid password, at least 6 characters.'
+          onInput={inputHandler}
+        />
+        {/* {!isLoginMode ? (
           <div className='form-group'>
-            <input
-              type='password'
+            <Input
+              element='input'
               id='confirmPassword'
-              name='confirmPassword'
-              value={formData.confirmPassword}
-              onChange={changeHandler}
-              required
-              className='input-field'
-              placeholder='Repeat your password'
+              type='password'
+              label='Confirm Password'
+              validators={['passwordMatch']}
+              errorText='Repeat your password.'
+              onInput={inputHandler}
             />
             {!passwordMatch && (
               <span className='error-message'>Your password and confirmation password must match.</span>
@@ -166,8 +135,8 @@ const Auth = () => {
                 type='checkbox'
                 id='agreeTerms'
                 name='agreeTerms'
-                checked={formData.agreeTerms}
-                onChange={changeHandler}
+                //checked={formData.agreeTerms}
+                //onChange={changeHandler}
                 required
               />
               <span className='checkbox-custom'></span>I agree to the terms and conditions
@@ -175,8 +144,12 @@ const Auth = () => {
           </div>
         ) : (
           ''
-        )}
-        <button type='submit' className='signin-button'>
+        )} */}
+        <button
+          type='submit'
+          disabled={!formState.isValid}
+          className={`signin-button ${!formState.isValid ? 'disabled' : ''}`}
+        >
           {isLoginMode ? 'Sign In' : 'Register'}
         </button>
         <div className='separator'>
@@ -224,13 +197,198 @@ const Auth = () => {
         />
       </div>
       <p>
-        {isLoginMode ? ' Not registered yet?  ' : 'Already have an account?  '}
+        {isLoginMode ? ' Not registered yet?  ' : 'Already have an account?  '}{' '}
         <a onClick={switchModeHandler} role='button'>
-          {isLoginMode ? 'Sign Up!' : 'Log In!'}
-        </a>
+          {isLoginMode ? 'Sign Up!' : 'Log In!'}{' '}
+        </a>{' '}
       </p>
     </div>
   );
 };
 
 export default Auth;
+
+// import React, { useState, useContext } from 'react';
+// import { GoogleLogin } from 'react-google-login';
+// import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+// import AppleLogin from 'react-apple-login';
+// import { FaGoogle, FaFacebook, FaApple } from 'react-icons/fa';
+// import AuthContext from '../../shared/context/auth-context';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+// import './Auth.css';
+
+// const Auth = () => {
+//   const auth = useContext(AuthContext);
+
+//   const [isLoginMode, setIsLoginMode] = useState(true);
+//   const [passwordMatch, setPasswordMatch] = useState(true);
+
+//   const [formData, setFormData] = useState({
+//     firstName: '',
+//     lastName: '',
+//     email: '',
+//     password: '',
+//     confirmPassword: '',
+//     agreeTerms: false,
+//   });
+
+//   const switchModeHandler = () => {
+//     setIsLoginMode(prevMode => !prevMode);
+//   };
+
+//   const changeHandler = event => {
+//     const { name, value, type, checked } = event.target;
+//     const newValue = type === 'checkbox' ? checked : value;
+
+//     setFormData(prevFormData => ({
+//       ...prevFormData,
+//       [name]: newValue,
+//     }));
+
+//     if (name === 'confirmPassword') {
+//       setPasswordMatch(formData.password === value);
+//     }
+//   };
+
+//   const togglePasswordVisibility = () => {
+//     setFormData(prevFormData => ({
+//       ...prevFormData,
+//       showPassword: !prevFormData.showPassword,
+//     }));
+//   };
+
+// const responseGoogle = response => {
+//   console.log(response);
+// };
+
+// const responseFacebook = response => {
+//   console.log(response);
+// };
+
+// const responseApple = response => {
+//   console.log(response);
+// };
+
+//   const submitHandler = event => {
+//     event.preventDefault();
+
+//     if (formData.password !== formData.confirmPassword) {
+//       setPasswordMatch(false);
+//       return;
+//     }
+
+//     // fetch('http://localhost:5000/users/register',{
+//     //   method: 'POST',
+//     //   headers: {
+//     //     'Content-Type': 'application/json'
+//     //   },
+//     //   body: JSON.stringify({
+//     //     name,
+//     //     email,
+//     //     password
+//     //   })
+//     // })
+
+//     console.log(formData);
+//     auth.login();
+//   };
+
+//   return (
+//     <div className='signin-form-container'>
+//       {isLoginMode ? <h2>Sign In</h2> : <h2>Create an Account</h2>}
+//       <form className='signin-form' onSubmit={submitHandler}>
+//         {!isLoginMode ? (
+//           <div className='form-group'>
+//             {/*<label htmlFor="firstName">First Name</label>*/}
+//             <input
+//               element='input'
+//               id='name'
+//               //type=''Yor Name''
+//               label='name'
+//               //value={formData.fullName}
+//               //onChange={handleChange}
+//               required
+//               //className='registration-form-input'
+//               className='input-field'
+//               placeholder='Your Name'
+//               onInput={changeHandler}
+//               // onInvalid={e => e.target.setCustomValidity('Please enter your name')}
+//               // onInput={e => e.target.setCustomValidity('')}
+//             />
+//           </div>
+//         ) : (
+//           ''
+//         )}
+//         <div className='form-group'>
+//           <input
+//             element='input'
+//             id='email'
+//             type='email'
+//             label='E-Mail'
+//             required
+//             className='input-field'
+//             placeholder='Email Address'
+//             onInput={changeHandler}
+//           />
+//         </div>
+//         <div className='form-group'>
+//           <input
+//             element='input'
+//             id='password'
+//             type={formData.showPassword ? 'text' : 'password'}
+//             label='Password'
+//             required
+//             className='input-field'
+//             placeholder='Password'
+//             onInput={changeHandler}
+//           />
+//           <FontAwesomeIcon
+//             icon={formData.showPassword ? faEye : faEyeSlash}
+//             className='password-icon'
+//             onClick={togglePasswordVisibility}
+//           />
+//         </div>
+//         {!isLoginMode ? (
+//           <div className='form-group'>
+//             <input
+//               type='password'
+//               id='confirmPassword'
+//               name='confirmPassword'
+//               value={formData.confirmPassword}
+//               onChange={changeHandler}
+//               required
+//               className='input-field'
+//               placeholder='Repeat your password'
+//             />
+//             {!passwordMatch && (
+//               <span className='error-message'>Your password and confirmation password must match.</span>
+//             )}
+//           </div>
+//         ) : (
+//           ''
+//         )}
+//         {!isLoginMode ? (
+//           <div className='form-group'>
+//             <label htmlFor='agreeTerms' className='checkbox-label'>
+//               <input
+//                 type='checkbox'
+//                 id='agreeTerms'
+//                 name='agreeTerms'
+//                 checked={formData.agreeTerms}
+//                 onChange={changeHandler}
+//                 required
+//               />
+//               <span className='checkbox-custom'></span>I agree to the terms and conditions
+//             </label>
+//           </div>
+//         ) : (
+//           ''
+//         )}
+
+//     </div>
+//   );
+// };
+
+// export default Auth;

92d4f18fc8b3a7ce44b101ac8a06f245ce7d9792
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-13 21:28:00 +0200

    Registration connected to the backend

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -53,27 +53,48 @@ const Auth = () => {
     setIsLoginMode(prevMode => !prevMode);
   };
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async event => {
     event.preventDefault();
 
-    if (!passwordMatch) {
-      return;
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch('http://localhost:5000/users/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
     }
 
-    console.log(formState.inputs);
     auth.login();
   };
 
-  const responseGoogle = response => {
-    console.log(response);
+  const successGoogleHandler = response => {
+    //auth.login();
+  };
+
+  const failureGoogleHandler = response => {
+    //console.log(response);
   };
 
   const responseFacebook = response => {
-    console.log(response);
+    //console.log(response);
   };
 
   const responseApple = response => {
-    console.log(response);
+    // console.log(response);
   };
 
   return (
@@ -161,8 +182,8 @@ const Auth = () => {
       <div className='signin-options'>
         <GoogleLogin
           clientId={process.env.REACT_APP_CLIENT_ID}
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
+          onSuccess={successGoogleHandler}
+          onFailure={failureGoogleHandler}
           cookiePolicy={'single_host_origin'}
           render={renderProps => (
             <button className='signin-option google-login' onClick={renderProps.onClick}>
@@ -207,188 +228,3 @@ const Auth = () => {
 };
 
 export default Auth;
-
-// import React, { useState, useContext } from 'react';
-// import { GoogleLogin } from 'react-google-login';
-// import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
-// import AppleLogin from 'react-apple-login';
-// import { FaGoogle, FaFacebook, FaApple } from 'react-icons/fa';
-// import AuthContext from '../../shared/context/auth-context';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-
-// import './Auth.css';
-
-// const Auth = () => {
-//   const auth = useContext(AuthContext);
-
-//   const [isLoginMode, setIsLoginMode] = useState(true);
-//   const [passwordMatch, setPasswordMatch] = useState(true);
-
-//   const [formData, setFormData] = useState({
-//     firstName: '',
-//     lastName: '',
-//     email: '',
-//     password: '',
-//     confirmPassword: '',
-//     agreeTerms: false,
-//   });
-
-//   const switchModeHandler = () => {
-//     setIsLoginMode(prevMode => !prevMode);
-//   };
-
-//   const changeHandler = event => {
-//     const { name, value, type, checked } = event.target;
-//     const newValue = type === 'checkbox' ? checked : value;
-
-//     setFormData(prevFormData => ({
-//       ...prevFormData,
-//       [name]: newValue,
-//     }));
-
-//     if (name === 'confirmPassword') {
-//       setPasswordMatch(formData.password === value);
-//     }
-//   };
-
-//   const togglePasswordVisibility = () => {
-//     setFormData(prevFormData => ({
-//       ...prevFormData,
-//       showPassword: !prevFormData.showPassword,
-//     }));
-//   };
-
-// const responseGoogle = response => {
-//   console.log(response);
-// };
-
-// const responseFacebook = response => {
-//   console.log(response);
-// };
-
-// const responseApple = response => {
-//   console.log(response);
-// };
-
-//   const submitHandler = event => {
-//     event.preventDefault();
-
-//     if (formData.password !== formData.confirmPassword) {
-//       setPasswordMatch(false);
-//       return;
-//     }
-
-//     // fetch('http://localhost:5000/users/register',{
-//     //   method: 'POST',
-//     //   headers: {
-//     //     'Content-Type': 'application/json'
-//     //   },
-//     //   body: JSON.stringify({
-//     //     name,
-//     //     email,
-//     //     password
-//     //   })
-//     // })
-
-//     console.log(formData);
-//     auth.login();
-//   };
-
-//   return (
-//     <div className='signin-form-container'>
-//       {isLoginMode ? <h2>Sign In</h2> : <h2>Create an Account</h2>}
-//       <form className='signin-form' onSubmit={submitHandler}>
-//         {!isLoginMode ? (
-//           <div className='form-group'>
-//             {/*<label htmlFor="firstName">First Name</label>*/}
-//             <input
-//               element='input'
-//               id='name'
-//               //type=''Yor Name''
-//               label='name'
-//               //value={formData.fullName}
-//               //onChange={handleChange}
-//               required
-//               //className='registration-form-input'
-//               className='input-field'
-//               placeholder='Your Name'
-//               onInput={changeHandler}
-//               // onInvalid={e => e.target.setCustomValidity('Please enter your name')}
-//               // onInput={e => e.target.setCustomValidity('')}
-//             />
-//           </div>
-//         ) : (
-//           ''
-//         )}
-//         <div className='form-group'>
-//           <input
-//             element='input'
-//             id='email'
-//             type='email'
-//             label='E-Mail'
-//             required
-//             className='input-field'
-//             placeholder='Email Address'
-//             onInput={changeHandler}
-//           />
-//         </div>
-//         <div className='form-group'>
-//           <input
-//             element='input'
-//             id='password'
-//             type={formData.showPassword ? 'text' : 'password'}
-//             label='Password'
-//             required
-//             className='input-field'
-//             placeholder='Password'
-//             onInput={changeHandler}
-//           />
-//           <FontAwesomeIcon
-//             icon={formData.showPassword ? faEye : faEyeSlash}
-//             className='password-icon'
-//             onClick={togglePasswordVisibility}
-//           />
-//         </div>
-//         {!isLoginMode ? (
-//           <div className='form-group'>
-//             <input
-//               type='password'
-//               id='confirmPassword'
-//               name='confirmPassword'
-//               value={formData.confirmPassword}
-//               onChange={changeHandler}
-//               required
-//               className='input-field'
-//               placeholder='Repeat your password'
-//             />
-//             {!passwordMatch && (
-//               <span className='error-message'>Your password and confirmation password must match.</span>
-//             )}
-//           </div>
-//         ) : (
-//           ''
-//         )}
-//         {!isLoginMode ? (
-//           <div className='form-group'>
-//             <label htmlFor='agreeTerms' className='checkbox-label'>
-//               <input
-//                 type='checkbox'
-//                 id='agreeTerms'
-//                 name='agreeTerms'
-//                 checked={formData.agreeTerms}
-//                 onChange={changeHandler}
-//                 required
-//               />
-//               <span className='checkbox-custom'></span>I agree to the terms and conditions
-//             </label>
-//           </div>
-//         ) : (
-//           ''
-//         )}
-
-//     </div>
-//   );
-// };
-
-// export default Auth;

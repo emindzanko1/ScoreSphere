9dd444aa2ff49c9e867c3508f0a57954cd0b74d0
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-14 17:08:05 +0200

    League page displays the table

--- frontend/src/leagues/components/League.js
+++ frontend/src/leagues/components/League.js
@@ -3,30 +3,16 @@ import { Link, useParams, useNavigate } from 'react-router-dom';
 import Table from './Table';
 import LeagueTable from './LeagueTable';
 import { clubs } from '../../clubs/pages/Clubs';
+import LoadingSpinner from '../../shared/UI/LoadingSpinner';
 
 import './League.css';
-
-const capitalizeWords = str => {
-  const smallWords = ['and', 'is', 'of'];
-  return str
-    .split('-')
-    .map((word, index) =>
-      smallWords.includes(word.toLowerCase()) && index !== 0
-        ? word.toLowerCase()
-        : word.charAt(0).toUpperCase() + word.slice(1)
-    )
-    .join(' ');
-};
 
 const League = props => {
   const [activeTable, setActiveTable] = useState('table');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
-  // const [selectedLeague, setSelectedLeague] = useState();
-  const { leagues } = props;
+  const [selectedLeague, setSelectedLeague] = useState();
   const { country, league } = useParams();
-  const capitalizedCountry = capitalizeWords(country);
-  const navigate = useNavigate();
 
   const handleFixturesClick = () => {
     setActiveTable('table');
@@ -38,73 +24,69 @@ const League = props => {
     //navigate(`./table`);
   };
 
-  // useEffect(() => {
-  //   const sendRequest = async () => {
-  //     try {
-  //       setIsLoading(true);
-  //       const response = await fetch('http://localhost:5000/:country/:league');
-
-  //       const responseData = await response.json();
-  //       console.log(responseData);
-  //       setSelectedLeague(responseData);
-
-  //       if (!response.ok) {
-  //         throw new Error(responseData.message);
-  //       }
-  //       setIsLoading(false);
-  //     } catch (error) {
-  //       setError(error.message);
-  //     }
-  //     setIsLoading(false);
-  //   };
-  //   sendRequest();
-  // }, []);
-
-  const selectedLeague = leagues.find(l => l.name === country && l.title === league);
-
   useEffect(() => {
-    if (!selectedLeague) {
-      navigate(`/`);
-    }
-  }, [selectedLeague, navigate]);
+    const sendRequest = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:5000/${country}/${league}`);
 
-  if (!selectedLeague) {
-    return null;
-  }
+        const responseData = await response.json();
+        setSelectedLeague(responseData.league);
 
-  const leagueClubs = clubs.filter(club => club.leagueId === selectedLeague.id);
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, [country, league]);
+
+  const leagueClubs = clubs.filter(club => club.leagueId === 'l2');
 
   return (
-    <div className='league-title'>
-      <h2>Welcome to {capitalizedCountry}!</h2>
-      <div className='league-container'>
-        <div className='button-container'>
-          <button onClick={handleFixturesClick} className={activeTable === 'table' ? 'active' : ''}>
-            Fixtures
-          </button>
-          <button onClick={handleTableClick} className={activeTable === 'leagueTable' ? 'active' : ''}>
-            Table
-          </button>
+    <React.Fragment>
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
         </div>
-        {activeTable === 'table' ? (
-          <Table
-            key={selectedLeague.id}
-            id={selectedLeague.id}
-            name={selectedLeague.name}
-            title={selectedLeague.title}
-            clubs={leagueClubs}
-            image={selectedLeague.image}
-          />
-        ) : (
-          <LeagueTable league={selectedLeague} teams={clubs} image={selectedLeague.image} />
-        )}
-        <Link to='/' className='link'>
-          <div className='button-container'>
-            <button>Homepage</button>
+      )}
+      {!isLoading && selectedLeague && (
+        <div className='league-title'>
+          <h2>Welcome to {selectedLeague.name}!</h2>
+          <div className='league-container'>
+            <div className='button-container'>
+              <button onClick={handleFixturesClick} className={activeTable === 'table' ? 'active' : ''}>
+                Fixtures
+              </button>
+              <button onClick={handleTableClick} className={activeTable === 'leagueTable' ? 'active' : ''}>
+                Table
+              </button>
+            </div>
+            {activeTable === 'table' ? (
+              <Table
+                key={selectedLeague.id}
+                id={selectedLeague.id}
+                name={selectedLeague.name}
+                title={selectedLeague.title}
+                clubs={leagueClubs}
+                image={selectedLeague.image}
+              />
+            ) : (
+              <LeagueTable league={selectedLeague} teams={clubs} image={selectedLeague.image} />
+            )}
+            <Link to='/' className='link'>
+              <div className='button-container'>
+                <button>Homepage</button>
+              </div>
+            </Link>
           </div>
-        </Link>
-      </div>
-    </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 

7d15c436ed3909d64d2137f069c0a3315abf880b
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-14 14:43:25 +0200

    Revert "Login connected to the backend"
    This reverts commit bc02ef5e99fbe9c5220cab19746c9da3f0b7639b.

--- frontend/src/leagues/pages/Leagues.js
+++ frontend/src/leagues/pages/Leagues.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import LeaguesList from '../components/LeaguesList';
 import axios from 'axios';
 import Brazil from '../../images/countries/Flag_of_Brazil.svg.png';
@@ -9,7 +9,6 @@ import Netherlands from '../../images/countries/Flag_of_Netherlands.svg.png';
 import Spain from '../../images/countries/Flag_of_Spain.svg.png';
 import Portugal from '../../images/countries/Flag_of_Portugal.svg.png';
 import LeaguesTables from '../components/LeaguesTables';
-import LoadingSpinner from '../../shared/UI/LoadingSpinner';
 
 export const leagues = [
   {
@@ -57,49 +56,11 @@ export const leagues = [
 ];
 
 const Leagues = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [loadedLeagues, setLoadedLeagues] = useState();
-
-  useEffect(() => {
-    const sendRequest = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch('http://localhost:5000/leagues');
-
-        const responseData = await response.json();
-        setLoadedLeagues(responseData.leagues);
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
-        setIsLoading(false);
-      } catch (error) {
-        setError(error.message);
-      }
-      setIsLoading(false);
-    };
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
-
   return (
-    <React.Fragment>
-      {isLoading && (
-        <div className='center'>
-          <LoadingSpinner />
-        </div>
-      )}
-      {!isLoading && loadedLeagues && (
-        <div className='leagues-container'>
-          <LeaguesList items={loadedLeagues} />
-          <LeaguesTables items={loadedLeagues} />
-        </div>
-      )}
-    </React.Fragment>
+    <div className='leagues-container'>
+      <LeaguesList items={leagues} />
+      <LeaguesTables items={leagues} />
+    </div>
   );
 };
 

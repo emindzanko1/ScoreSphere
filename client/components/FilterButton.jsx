b41b4d447dd359722fbadb9b9077fb06b11e8d70
emindzanko1 <edzanko1@etf.unsa.ba>
2024-09-10 20:57:22 +0200

    Implemented button for switching modes, added some stylings

--- client/components/FilterButton.jsx
+++ client/components/FilterButton.jsx
@@ -11,36 +11,34 @@ export default function FilterButton() {
   };
 
   return (
-    <div className='main-content'>
-      <div className='main-controls'>
-        {['All', 'Live', 'Done', 'Schedule'].map((label, index) => (
-          <button
-            key={index}
-            className={activeButton === index ? 'active-button' : 'button'}
-            onClick={() => handleButtonClick(index)}
-          >
-            {label}
-          </button>
-        ))}
-        <div className='calendar__datepicker'>
-          <button
-            className='calendar__navigation--yesterday'
-            title='The previous day'
-            onClick={() => setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() - 1)))}
-          >
-            &lt;
-            {/* <MdKeyboardArrowLeft /> */}
-          </button>
-          <CalendarDropdown date={currentDate} setDate={setCurrentDate} />
-          <button
-            className='calendar__navigation--tomorrow'
-            title='The next day'
-            onClick={() => setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + 1)))}
-          >
-            &gt;
-            {/* <MdKeyboardArrowRight /> */}
-          </button>
-        </div>
+    <div className='main-controls'>
+      {['All', 'Live', 'Done', 'Schedule'].map((label, index) => (
+        <button
+          key={index}
+          className={activeButton === index ? 'active-button' : 'button'}
+          onClick={() => handleButtonClick(index)}
+        >
+          {label}
+        </button>
+      ))}
+      <div className='calendar__datepicker'>
+        <button
+          className='calendar__navigation--yesterday'
+          title='The previous day'
+          onClick={() => setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() - 1)))}
+        >
+          &lt;
+          {/* <MdKeyboardArrowLeft /> */}
+        </button>
+        <CalendarDropdown date={currentDate} setDate={setCurrentDate} />
+        <button
+          className='calendar__navigation--tomorrow'
+          title='The next day'
+          onClick={() => setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + 1)))}
+        >
+          &gt;
+          {/* <MdKeyboardArrowRight /> */}
+        </button>
       </div>
     </div>
   );
---
+++ client/components/ThemeSwitch.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { BsSun, BsMoon } from 'react-icons/bs';
+import '../styles/ThemeSwitch.css';
+
+const ThemeSwitch = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+      window.localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      setTheme('light');
+      window.localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+
+    if (localTheme) {
+        setTheme(localTheme);
+
+        if (localTheme === 'dark') {
+          document.documentElement.classList.add('dark');
+        }
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme('dark');
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
+
+  return <button onClick={toggleTheme} className='toggle'>{theme === 'light' ? <BsSun /> : <BsMoon />}</button>;
+};
+
+export default ThemeSwitch;

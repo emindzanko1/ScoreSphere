beb0aa6e190bbfa7eec48b1f2dbf4904ef06e8d9
emindzanko1 <edzanko1@etf.unsa.ba>
2024-09-11 13:13:25 +0200

    Added route protection and token expiration

--- client/pages/Root.jsx
+++ client/pages/Root.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import { getTokenDuration } from '../util/auth';
 
-const RootLayout = () => (
-  <>
-    <Header />
-    <Outlet />
-    <Footer />
-  </>
-);
+const RootLayout = () => {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) return;
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 export default RootLayout;
